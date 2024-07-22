6921b8e359eab3b40f938cc5d09afb081c07b05b
Clark Chen <boo56mu@gmail.com>
2024-07-22 20:45:11 +0800

    feat: Support postgres client

---
+++ main/infrastructure/postgres/client.go
@@ -0,0 +1,27 @@
+package postgres
+
+import (
+    "database/sql"
+    "fmt"
+    _ "github.com/lib/pq"
+    "log"
+    "tradingACE/main/infrastructure/server"
+)
+
+func Init() *sql.DB {
+    database := server.SystemConfig.Database
+    psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=%s timezone=%s",
+        database.Host, database.Port, database.User, database.Password, database.Name, database.SSLMode, database.TimeZone)
+    client, err := sql.Open("postgres", psqlInfo)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    err = client.Ping()
+    if err != nil {
+        log.Fatal("Could not connect to PostgreSQL", err)
+    }
+
+    log.Println("Connected to PostgreSQL")
+    return client
+}
